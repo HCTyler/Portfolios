f0818c6f8ad70117eb39f6900b70ecf624e37a56
Edgar Fernandez <edgar.fernand@outlook.com>
2022-06-08 12:21:32 -0700

    changed readme

--- src/pages/AboutMe/About.js
+++ src/pages/AboutMe/About.js
@@ -8,7 +8,7 @@ function About() {
     <h1>About Me</h1>
     <p>
       Hello world! My name is Edgar Fernandez. I am a junior web developer based out on San Diego, California and I like building things on the web.
-      I recently graduated from a coding bootcamp through the University of California- San Diego, and through there I've learned alot. 
+      I recently graduated from a coding bootcamp through the University of California- San Diego, and through there I've learned alot.
     </p>
   </div>
   )
