659a451f72dc71fd7007aae2909aac29ecd66b1a
Edgar Fernandez <edgar.fernand@outlook.com>
2022-11-10 12:43:42 -0800

    added bootstrap link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import React from "react"
 import "./Footer"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Footer() {
 
